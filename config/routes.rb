eba8e39be2f2a2144b5e2c9c4f12f28b6c04c293
Bryan Cole <brycole@gmail.com>
2020-01-21 20:50:12 +0000

    added new restaurant form and generated reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "controller#action"
+  root to: 'restaurants#index'
 
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  end
 end
